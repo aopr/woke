1fe1401551e6322b6d2f49cfb57a85db239ded98
Steven Gilliam <stevengilliamjr@gmail.com>
2016-08-30 21:01:54 -0400

    Added new create index and params methods to SavedFeedliesController

--- app/controllers/saved_feedlies_controller.rb
+++ app/controllers/saved_feedlies_controller.rb
@@ -1,2 +1,20 @@
 class SavedFeedliesController < ApplicationController
+  def new
+    @saved_feedly = Saved_feedly.new
+  end
+
+  def create
+    @saved_feedly = Saved_feedly.create!(saved_feedly_params)
+  end
+
+  def index
+    @saved_feedlies = Saved_feedly.all
+  end
+
+private
+  def saved_feedly_params
+    params.require(:saved_feedly).permit(:source, :headline, :url, :pic, :keywords, :lead, :topic_id, :story_type, :show_pic, :home_feature, :home_main, :home_main_pic, :home_right, :topic_feature, :topic_pic)
+  end
+end
+
 end
