2f167b4c251c02cfae04f93ea272d68c1999751d
Steven Gilliam <stevengilliamjr@gmail.com>
2016-08-31 12:55:36 -0400

    added ogin action and redirect to root for Users#Create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      # Log the user in and redirect to root
+      log_in user
+      redirect_to root
     else
       flash[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
