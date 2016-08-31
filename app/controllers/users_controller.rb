344205ec48b458f0cc9dad2cd893a61de757bdbb
Steven Gilliam <stevengilliamjr@gmail.com>
2016-08-31 12:36:05 -0400

    Added User show view Added user signup for to Users/new Added show and update method to User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
+  
   def create
     user = User.new(user_params)
     if user.save
@@ -9,18 +10,17 @@ class UsersController < ApplicationController
       redirect_to '/'
     else
       redirect_to '/signup'
+    end
   end
-end
+
+  def show
+    @user = User.find(params[:id])
   end
 
-
-
-
-
-
-
-
-
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
 
   private
     def user_params
